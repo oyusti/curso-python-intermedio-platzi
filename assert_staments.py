d2952222bf0b8938503376ade06ee37f10287ca0
Oscar Yusti <oyusti@gmail.com>
2022-05-10 10:55:02 -0400

    Termiando el archivo assert_staments

---
+++ assert_staments.py
@@ -0,0 +1,16 @@
+#Programa que calcula los divisores de un numero e imprime mensajes de error cuando
+#se inserta un valor que no sea un numero asi como tambien, un valor cuando 
+#se ingresa un numero negativo
+def divisors(num):
+    divisors = [i for i in range(1, num+1) if num % i==0]
+    return divisors
+#Con assert confirma la expresion y despues de la coma va lo que queremos que haga siempre que la expresion sea False
+def run():
+    print("Programa que calcula los divisores de un numero")
+    value_entry = input("Ingrese un numero: ")
+    assert value_entry.isnumeric() and int(value_entry) >= 0, "Debes ingresar un numero positivo"
+    print(divisors(int(value_entry)))
+            
+    
+if __name__== '__main__':
+    run()
