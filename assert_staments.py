3845c1bb4e055c2134f6611b8f9b6be9118bca38
Oscar Yusti <oyusti@gmail.com>
2022-05-10 19:50:23 -0400

    assert_statement terminado

--- assert_staments.py
+++ assert_staments.py
@@ -8,6 +8,9 @@ def divisors(num):
 def run():
     print("Programa que calcula los divisores de un numero")
     value_entry = input("Ingrese un numero: ")
+#El metodo isnumeric se usa para verificar si un caracter o cadena de caracteres es un numero o no, Este metodo no admite signos
+# En este caso si colocaramos (-30) como ejemplo nos daria false porque aunque 30 es un numero al colocarle
+# el guion de negativo, el metodo nos devolveria false.   
     assert value_entry.isnumeric(), "Debes ingresar un numero positivo"
     print(divisors(int(value_entry)))
             
