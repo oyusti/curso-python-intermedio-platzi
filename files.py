973334fce45c1861de756f8036891b23250446bb
Oscar Yusti <oyusti@gmail.com>
2022-05-10 22:18:51 -0400

    Commit a ultimas actualziaciones

---
+++ files.py
@@ -0,0 +1,21 @@
+def read():
+    numbers = []
+    with open("./files/numbers.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            numbers.append(int(line))
+        print(numbers)
+
+def write():
+    names = ["Facundo", "Miguel", "Pepe", "Christian"]
+    with open("./files/names.txt", "w", encoding="utf-8") as f:
+        for name in names:
+            f.write(name)
+            f.write("\n")
+        f.close
+
+def run():
+    read()
+    write()
+
+if __name__== '__main__':
+    run()
